878bf499d35986802edee8ca9311a4a67670b5bf
sahil-raj <sahilrajaaru@gmail.com>
2024-04-07 01:41:30 +0530

    update: contract dev

--- contracts/ForgEvent.sol
+++ contracts/ForgEvent.sol
@@ -6,6 +6,11 @@ pragma solidity ^0.8.4;
 contract ForgEvent {
     constructor() {}
 
+    //events
+    //event to emit when event is created
+    event EventCreated(address eventCreator, string eventName, uint eventId);
+
+    //structure of each event
     struct Forg {
         string event_name;
         uint start_timestamp;
@@ -15,9 +20,15 @@ contract ForgEvent {
         address creator;
         address[] organizers;
         bool active;
+        string eventJsonData;//other data related to event
     }
 
-    modifier checkForCreation(uint _startTimestamp, uint _endTimestamp, uint _ticketCount) {
+    mapping(bytes32 => Forg) eventMapping;
+
+    //checks for createEvent function
+    modifier checkForCreation(string memory _eventName, uint _startTimestamp, uint _endTimestamp, uint _ticketCount) {
+        //must have a name
+        require(bytes(_eventName).length != 0, "Must have a name");
         //event can't be in past
         require(_startTimestamp >= block.timestamp && _endTimestamp >= block.timestamp, "Event must be in future");
         //end time must be after start
@@ -27,7 +38,15 @@ contract ForgEvent {
         _;
     }
 
-    function createEvent(uint _eventName, uint _startTimestamp, uint _endTimestamp, uint _ticketCount, uint _ticketPrice, address[] memory _organizers, bool _active) checkForCreation(_startTimestamp, _endTimestamp, _ticketCount) public returns(bool) {
+    function createEvent(string memory _eventName, uint _startTimestamp, uint _endTimestamp, uint _ticketCount, uint _ticketPrice, address[] memory _organizers, bool _active, string memory _eventJsonData) checkForCreation(_eventName, _startTimestamp, _endTimestamp, _ticketCount) public returns(bytes32) {
+        //for creating unique id of each event
+        bytes32 uid = keccak256(abi.encode(block.timestamp, msg.sender, _eventName, _startTimestamp, _endTimestamp));
 
+        //obj of the Forg type to create an event
+        Forg memory e = Forg(_eventName, _startTimestamp, _endTimestamp, _ticketCount, _ticketPrice, msg.sender, _organizers, _active, _eventJsonData);
+
+        //map the uid to each event
+        eventMapping[uid] = e;
+        return uid;
     }
 }
